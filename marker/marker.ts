db10aa5091a3d2dfb93f4186470cdc3dd229b73e
Ben Greene <b.greene@analyticsengines.com>
2017-01-17 15:45:52 +0000

    Remove a lot of the group service. Remove a lot of the marker element.

--- marker/marker.ts
+++ marker/marker.ts
@@ -12,107 +12,66 @@ import 'rxjs/add/operator/catch';
 import * as L from 'leaflet';
 
 @Component({
-  moduleId: module.id.toString(),
-  selector: 'marker-element',
-  templateUrl: 'marker.html',
-  styleUrls: ['marker.css'],
-  providers: [PopupService]
+    moduleId: module.id.toString(),
+    selector: 'marker-element',
+    templateUrl: 'marker.html',
+    styleUrls: ['marker.css'],
+    providers: [PopupService]
 })
 
 export class MarkerElement {
-  @Input() lat: number = 52.6;
-  @Input() lon: number = -1.1;
-  @Input() mouseover: string = "";
-  @Input() onclick: string = "";
-  @Input() iconUrl: string = "";
+    @Input() lat: number = 52.6;
+    @Input() lon: number = -1.1;
+    @Input() mouseover: string = "";
+    @Input() popup: any;
+    @Input() popupOptions: L.PopupOptions;
+    @Input() onclick: string = "";
+    @Input() iconUrl: string = "";
+    @Input() icon: L.Icon;
+    @Input() title: string;
 
-  constructor(
-    private mapService: MapService,
-    private groupService: GroupService,
-    private popupService: PopupService,
-    private http: Http,
-    @Optional() private LeafletElement?: LeafletElement,
-    @Optional() private LeafletGroup?: LeafletGroup) {
-  }
+    constructor(
+        private mapService: MapService,
+        private groupService: GroupService,
+        private popupService: PopupService,
+        @Optional() private LeafletElement?: LeafletElement,
+        @Optional() private LeafletGroup?: LeafletGroup) {
+    }
 
-  ngOnInit() {
-    var model = this;
-    if (this.LeafletElement || this.LeafletGroup) {
+    ngOnInit() {
+        if (this.LeafletElement || this.LeafletGroup) {
 
-      let map = this.mapService.getMap();
+            let map = this.mapService.getMap();
 
+            //trick to match the number of layers
+            if (this.LeafletGroup) {
+                this.groupService.increaseNumber();
+            }
 
-      var marker = null;
-
-      //trick to match the number of layers
-      if (this.LeafletGroup) {
-        this.groupService.increaseNumber();
-      }
-
-      if (this.iconUrl === "") {
-        marker = L.marker([this.lat, this.lon]);
-        this.createMarkerlayer(marker, map);
-      } else {
-        this.imageExists(this.iconUrl, function (exists) {
-
-          model.getImage().subscribe(
-            image => {
-              var img = document.createElement("img");
-              window.URL.createObjectURL(image.blob())
-              var reader = new FileReader();
-              reader.onload = function () {
-                img.src = reader.result;
-                var myIcon = L.icon({
-                  iconUrl: model.iconUrl,
-                  iconSize: [img.width, img.height],
-                  iconAnchor: [img.width / 2, img.height - 1],
-                  popupAnchor: [0, -img.height]
-                });
-                marker = L.marker([model.lat, model.lon], { icon: myIcon });
-                model.createMarkerlayer(marker, map);
-              }
-              reader.readAsDataURL(image.blob());
-            },
-            err => {
-              console.log(err);
+            var marker = L.marker([this.lat, this.lon], {
+                title: this.title,
+                icon: this.icon,
             });
-        });
-      }
-    } else {
-      console.warn("This marker-element will not be rendered \n the expected parent node of marker-element should be either leaf-element or leaflet-group");
+            if (this.popup) {
+                marker.bindPopup(this.popup, {
+                    offset: L.point(12, 6)
+                }).openPopup();
+            }
+
+            this.createMarkerlayer(marker, map);
+
+        }
     }
-  }
 
-  createMarkerlayer(marker, map) {
-    //add popup methods on element
-    this.popupService.enablePopup(this.mouseover, this.onclick, marker);
+    createMarkerlayer(marker, map) {
+        //add popup methods on element
+        // this.popupService.enablePopup(this.mouseover, this.onclick, marker);
 
-    //only if the parent is map should the marker-element should be directly added to the map
-    if (this.LeafletGroup) {
-      this.groupService.addOLayersToGroup(marker, map, this.mapService, this.LeafletGroup);
-    } else {
-      marker.addTo(map);
+        //only if the parent is map should the marker-element should be directly added to the map
+        if (this.LeafletGroup) {
+            this.groupService.addOLayersToGroup(marker, map, this.mapService, this.LeafletGroup);
+        } else {
+            marker.addTo(map);
+        }
     }
-  }
-
-  imageExists(url, callback) {
-    var img = new Image();
-    img.onload = function () { callback(true); };
-    img.onerror = function () { callback(false); };
-    img.src = url;
-  }
-
-  getImage(): Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
-    let options = new RequestOptions({
-      responseType: ResponseContentType.Blob,
-      headers: headers
-    });
-
-
-    return this.http.get(this.iconUrl, options)
-      .map((res: Response) => res)
-      .catch((error: any) => Observable.throw('Server error'));
-
-  }
 }
--- services/group.service.ts
+++ services/group.service.ts
@@ -9,7 +9,7 @@ export class GroupService {
     private layerGroup: Array<any> = [];
     private layerId: Array<any> = [];
     private layerGroupNumber: number = 0;
-    private group: any = {};
+    private group: L.LayerGroup;
 
     constructor(private guidService: GuidService) { }
 
@@ -17,39 +17,47 @@ export class GroupService {
         // Create a gId so that each overlay can be individually identified and modified by reference to gId ... BUT currently does not return gId to outside class.
         // It should be possible to combine gId and replace as you cannot do replace unless gId is provided. And if gId is provided then can assume you want to do a modify.
         // TODO: remove 'replace' option in call method in favour of only having gId
-
-        if (!gId) {
-            gId = this.guidService.newGuid();
-        }
-
-        // TODO: Consider to use https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes from ES7
-        if (this.layerId.indexOf(gId) === -1) {
-            this.layerId.push(gId);
-        }
+        //
+        // if (!gId) {
+        //     gId = this.guidService.newGuid();
+        // }
+        //
+        // // TODO: Consider to use https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes from ES7
+        // if (this.layerId.indexOf(gId) === -1) {
+        //     this.layerId.push(gId);
+        // }
         // Current implementation of adding a marker is to remove the layer for the group and re-create it from the array.
         // TODO: rewrite this to modify the layerGroup using .addLayers on the LayersGroup
         // Not clear what this if statement is aiming to achieve ? As this will trigger every time after initialisation of this.group so why not check for defined and/or remove the default initialisation to empty dict.
-        if (Object.keys(this.group).length !== 0) {
-            map.removeLayer(this.group);
-            if (replace) {
-                // TODO: Check if there is another method that allows to search for a layerId to be modified or removed (rather than trying to do a gId match and then reference the inde of the entry
-                if (this.layerId.indexOf(gId) !== -1) {
-                    this.layerGroup[this.layerId.indexOf(gId)] = overlay;
-                } else {
-                    this.layerGroup.push(overlay);
-                }
-            }
+        // if (Object.keys(this.group).length !== 0) {
+        //     map.removeLayer(this.group);
+        //     if (replace) {
+        //         // TODO: Check if there is another method that allows to search for a layerId to be modified or removed (rather than trying to do a gId match and then reference the inde of the entry
+        //         if (this.layerId.indexOf(gId) !== -1) {
+        //             this.layerGroup[this.layerId.indexOf(gId)] = overlay;
+        //         } else {
+        //             this.layerGroup.push(overlay);
+        //         }
+        //     }
+        // }
+        // if (!replace) {
+        //     // This will fail if replace has been selected AND the item does not exist in the layerId table.
+        //     this.layerGroup.push(overlay);
+        // }
+        // // This is re-creating the whole layerGroup every time a new marker is added. Consider to re-write with addLayer methods
+        // this.group = L.layerGroup(this.getLayerGroup());
+        // console.log("Layer Group:", this.group);
+        // // This adds the layer to the map EVERY time this method is called. It should just be added once and referenced after that
+        // this.group.addTo(map);
+        // mapService.addOverlay(this.getGroup(), group.name, group.globalId);
+
+        //If group does not exist then create it.
+        if (!this.group) {
+            this.group = L.layerGroup([])
+                .addTo(map);
+            mapService.addOverlay(this.getGroup(), group.name, group.globalId);
         }
-        if (!replace) {
-            // This will fail if replace has been selected AND the item does not exist in the layerId table.
-            this.layerGroup.push(overlay);
-        }
-        // This is re-creating the whole layerGroup every time a new marker is added. Consider to re-write with addLayer methods
-        this.group = L.layerGroup(this.getLayerGroup());
-        console.log("Layer Group:", this.group);
-        // This adds the layer to the map EVERY time this method is called. It should just be added once and referenced after that
-        this.group.addTo(map);
-        mapService.addOverlay(this.getGroup(), group.name, group.globalId);
+        this.group.addLayer(overlay);
     }
 
     public getObservableGroup() {
